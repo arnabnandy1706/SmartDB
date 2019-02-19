2885ed9ad8e7386b41310bef7a996d2a0398d8ee
root <root@k8-master.UCSBANG6.com>
2019-02-19 14:51:08 +0530

    Added Code in fetch.py

--- fetch.py
+++ fetch.py
@@ -29,5 +29,29 @@ MEM = mem.stdout.read().strip('\n')
 print("Memory: "+MEM)
 
 serial = subprocess.Popen('sudo cat /sys/devices/virtual/dmi/id/product_serial', stdout=subprocess.PIPE, shell=True)
-SERIAL = serial.stdout.read().split('\n')
-print("Seral Number; "+SERIAL)
+SERIAL = serial.stdout.read().split('\n')[0]
+print("Seral Number: "+ str(SERIAL))
+
+model = subprocess.Popen('sudo cat /sys/devices/virtual/dmi/id/product_name', stdout=subprocess.PIPE, shell=True)
+MODEL = model.stdout.read().split('\n')[0]
+print("Model: "+ str(MODEL))
+
+ip = subprocess.Popen('hostname -I', stdout=subprocess.PIPE, shell=True)
+IP = ip.stdout.read().split('\n')[0]
+print("IP(s): "+ str(IP))
+
+os = subprocess.Popen('/bin/uname -a', stdout=subprocess.PIPE, shell=True)
+OS = os.stdout.read().split('\n')[0]
+print("OS: "+OS)
+
+fileBuff = open("/tmp/details.txt","w")
+
+fileBuff.write("HOSTNAME="+HOSTNAME+"\n")
+fileBuff.write("CPU="+CPU+"\n")
+fileBuff.write("MEM="+MEM+"\n")
+fileBuff.write("SERIAL="+SERIAL+"\n")
+fileBuff.write("MODEL="+MODEL+"\n")
+fileBuff.write("IP="+IP+"\n")
+fileBuff.write("OS="+OS+"\n")
+
+fileBuff.close()
